ab584af8e89cc0cee1404bbaa6b3d0156971c83e
dboeloe <dboeloe>
2020-02-10 10:19:23 -0500

    Wrap up Wraper

--- config/config.example.rb
+++ config/config.example.rb
@@ -1,27 +1,35 @@
 Configuration.for('database') {
-  database_name ''
-  database_user ''
-  database_password ''
-  database_host ''
+  database_name 'scraper'
+  database_user 'dboeloe'
+  database_password 'dboeloe'
+  database_host 'localhost'
 }
 
 Configuration.for('elasticsearch') {
   es_host ''
   port ''
-  user ''
-  password ''
-  cloud_id ''
-  index_name ''
+  user 'elastic'
+  password 'sdfsfsdfds'
+  cloud_id 'ElasticCertifiedEngineer:asdadasd'
+  index_name '123_scraper'
+  es_source 'elastic_cloud'
 }
 
 Configuration.for('general') {
-  es_source ''
+  es_source 'elastic_cloud'
   use_heroku false
-  use_app_search true
+  use_app_search false
 }
 
 Configuration.for('app_search') {
   host_identifier ''
   api_key ''
   engine_name ''
+}
+
+Configuration.for('storage') {
+  region 'ap-southeast-1'
+  aws_key 'sdfsdfsdfsfsdf'
+  aws_secret_key 'sfsdfsdfsdfsdfs+khy8Uqinn95z4w4'
+  bucket_name 'jnny-fashion-cache'
 }
